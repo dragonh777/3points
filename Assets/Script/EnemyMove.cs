cf4b71f7332e81bece1b8ca4695d88279b2bf0b6
dragonh777 <dragonh077@gmail.com>
2020-02-22 19:13:13 +0900

    Hp, CoolTime

--- Assets/Script/EnemyMove.cs
+++ Assets/Script/EnemyMove.cs
@@ -8,15 +8,13 @@ public class EnemyMove : MonoBehaviour
     //public float EmoveSpeed;
     public float eBulletPosX;
     public float eBulletPosY;
-    public float eSkillPosX;
-    public float eSkillPosY;
     public float bulletDelay;
+    public float coolTime;
     public Slider healthBarSlider;
 
     public int EHpMax;
 
     public GameObject Bullet;
-    public GameObject Skill;
 
     public static int Hp;
 
@@ -38,15 +36,6 @@ public class EnemyMove : MonoBehaviour
     {
         Bspeed = Random.Range(-eBulletPosY, eBulletPosY);
         Instantiate(Bullet, new Vector3(BulletP, Bspeed, 0), Quaternion.identity);
-    }
-    public void Skill1Active()
-    {
-        Instantiate(Skill, new Vector3(eSkillPosX, 0, 0), Quaternion.identity);
-    }
-    IEnumerator skill1()
-    {
-        Instantiate(Skill, new Vector3(eSkillPosX, 0, 0), Quaternion.identity);
-        yield return new WaitForSeconds(0.0f);
     }
     void Update()
     {
--- Assets/Script/EnemySkill_1.cs
+++ Assets/Script/EnemySkill_1.cs
@@ -1,10 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemySkill_1 : MonoBehaviour
 {
     public float BulletSpeed;
+
+    public GameObject Skill;
+    public Image image;
+    public Button skillButton;
+    public float coolTime = 10.0f;
+    public bool isClicked = false;
+    float leftTime = 10.0f;
+    float speed = 5.0f;
 
     private Transform transform;
 
@@ -21,6 +30,33 @@ public class EnemySkill_1 : MonoBehaviour
     void Update()
     {
         transform.Translate(moveAmount);
+
+        if (isClicked)
+        {
+            if(leftTime > 0)
+            {
+                leftTime -= Time.deltaTime * speed;
+                if(leftTime < 0)
+                {
+                    leftTime = 0;
+                    if (skillButton)
+                        skillButton.enabled = true;
+                    isClicked = true;
+                }
+                float ratio = 1.0f - (leftTime / coolTime);
+                if (image)
+                    image.fillAmount = ratio;
+            }
+        }
+    }
+
+    public void StartCoolTime()
+    {
+        Instantiate(Skill, new Vector3(6, 0, 0), Quaternion.identity);
+        leftTime = coolTime;
+        isClicked = true;
+        if (skillButton)
+            skillButton.enabled = false;
     }
 
     void OnCollisionEnter2D(Collision2D collision)
