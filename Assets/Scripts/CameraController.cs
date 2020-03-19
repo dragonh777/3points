8e42d5c58fb73855abf8fc32fb0948724250646e
dragonh777 <dragonh077@gmail.com>
2020-03-20 02:02:17 +0900

    카메라 조정, 비율 조정, 바닥 추가
    카메라 이동, 카메라 범위 설정

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+
+    Transform playerT;
+    Transform transform;
+
+    public BoxCollider2D bound;
+
+    private Vector3 minBound;
+    private Vector3 maxBound;
+
+    private float halfWidth;
+    private float halfHeight;
+
+    private Camera theCamera;
+
+    private void Awake()
+    {
+        transform = GetComponent<Transform>();
+        playerT = player.transform;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theCamera = GetComponent<Camera>();
+        minBound = bound.bounds.min;
+        maxBound = bound.bounds.max;
+        halfHeight = theCamera.orthographicSize;
+        halfWidth = halfHeight * Screen.width / Screen.height;// 해상도
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(playerT.position.x, playerT.position.y + 2.5f, playerT.position.z - 10);
+
+        float clampedX = Mathf.Clamp(this.transform.position.x, minBound.x + halfWidth, maxBound.x - halfWidth);  //Clamp('값', '최솟값', '최댓값') 값이 무조건 최솟값 최댓값 사이만 나옴
+        float clampedY = Mathf.Clamp(this.transform.position.y, minBound.y + halfHeight, maxBound.y - halfHeight);
+
+        this.transform.position = new Vector3(clampedX, clampedY, this.transform.position.z);
+    }
+}
