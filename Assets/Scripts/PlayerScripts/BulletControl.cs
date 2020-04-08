b4be881039eedcee3550a5271b911499a8443e48
hangwoo <61936103+gkddn97@users.noreply.github.com>
2020-04-08 18:02:18 +0900

    Revert "캐릭터 대쉬딜레이, 마우스방향 총알발사, 주석추가"
    This reverts commit 85e58ab068270dbc9d18a2179fa145078c9c7b7d.

--- Assets/Scripts/PlayerScripts/BulletControl.cs
+++ Assets/Scripts/PlayerScripts/BulletControl.cs
@@ -5,43 +5,43 @@ using UnityEngine.UI;
 
 public class BulletControl : MonoBehaviour
 {
-    // Bullet프리팹 스크립트
-    // 구현된 기능
-    //마우스 방향으로 날아감
-    // 데미지, 속도 가지고 있음
-
-    // 구현해야할 기능
-    // 날아가는 방향으로 총알 회전시키기
-
     public float bulletSpeed = 30f;
     public int bulletDamage = 35;
 
     public static int bDamage;
 
-    Vector2 dir;
+    private Transform transform;
+    Vector3 moveAmount;
 
     // Start is called before the first frame update
-    void Start()    // Bullet프리팹 생성시 초기화
+    void Start()
     {
+        transform = GetComponent<Transform>();
         bDamage = bulletDamage;
-        dir = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - this.transform.position);
+        if (PlayerMove.pLeft)
+        {
+            moveAmount = bulletSpeed * Vector3.left * Time.deltaTime;
+        }
+        else if (!PlayerMove.pLeft)
+        {
+            moveAmount = bulletSpeed * Vector3.right * Time.deltaTime;
+        }
+
     }
 
     // Update is called once per frame
-    void FixedUpdate()  // Bullet프리팹 생성 후 날아가게 하기
+    void FixedUpdate()
     {
-        GetComponent<Transform>().transform.Translate(dir.normalized * bulletSpeed * Time.deltaTime);
-
-        Destroy(gameObject, 5); // 5초 후 자동 Destroy
+         transform.Translate(moveAmount);
     }
 
-    void OnCollisionEnter2D(Collision2D collision)  // 날아가다 충돌시
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Wall"|| collision.gameObject.tag == "Floor")   // 태그 Wall이나 Floor에닿으면 Destroy
+        if (collision.gameObject.tag == "Wall"|| collision.gameObject.tag == "RightWall"|| collision.gameObject.tag == "LeftWall")
         {
             Destroy(gameObject);
         }
-        else if (collision.gameObject.tag == "Enemy")   // 태그 Enemy에 닿으면
+        else if (collision.gameObject.tag == "Enemy")
         {
             Debug.Log("Hit");
             Destroy(gameObject);
