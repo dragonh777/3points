97a75010d3c23ccc78b67055a0f595bb6441a486
dragonh777 <60222545+dragonh777@users.noreply.github.com>
2020-06-27 17:28:20 +0900

    텍스트 추가

---
+++ Assets/Scripts/BackgroundScripts/TextControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class TextControl : MonoBehaviour
+{
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
